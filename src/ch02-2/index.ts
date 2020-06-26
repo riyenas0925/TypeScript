0076ce367b743b32e6c13e0cf01c55fc7d5d6e71
riyenas0925 <riyenas0925@gmail.com>
2020-06-26 14:12:54 +0000

    02-2 모듈 이해하기(모듈화 후)

--- src/ch02-2/index.ts
+++ src/ch02-2/index.ts
@@ -1,21 +1,5 @@
-let MAX_AGE = 100
-
-interface IPerson {
-    name: string
-    age: number
-}
-
-class Person implements IPerson {
-    constructor(public name: string, public age: number) {}
-}
-
-function makeRandomNumber(max: number = MAX_AGE): number {
-    return Math.ceil((Math.random() * max))
-}
-
-const makePerson = (
-    name: string,
-    age: number = makeRandomNumber()) => ({name, age})
+import IPerson from './person/IPerson'
+import {makePerson} from './person/Person'
 
 const testMakePerson = (): void => {
     let jane: IPerson = makePerson('jane')
