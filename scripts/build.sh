60b8dc8a0750619d8693742264add37a7f7f030f
riyenas0925 <riyenas0925@gmail.com>
2020-06-27 14:17:54 +0000

    ENH : TypeScript 디버깅 기능 설정

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-DIRS=`ls -l ./src/ | grep "^d"|awk '{print $9}'`
-PS3='Select Build TypeScript directory : '
-
-echo TypeScript src directory list
-echo
-
-select dir in ${DIRS}
-do
-    echo 
-    echo '$ sudo tsc && node ./dist/$dir'
-    sudo tsc && node ./dist/$dir
-break
-done
+sudo tsc
